f70125fa9eaf385dc5069365b387e08aab94ef58
abelb23 <abelb23@my.yorku.ca>
2025-11-14 07:12:26 -0500

    searched player functions and career totals functions finished

---
+++ backend/GetMethods.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from nba_api.stats import static
+from nba_api.stats import endpoints as end
+from nba_api.stats.static import players as pcs
+import pandas as pd
+
+app = FastAPI()
+
+@app.get("/search")
+def getSearchedPlayers(name: str):
+    names = pcs.get_players()
+    playerList = {}
+    for dicts in names:
+        if (name.lower() in dicts["full_name"].lower()):
+            playerList[dicts["full_name"]] = dicts["id"]
+
+    return playerList
+
+@app.get("/stats")
+def getCareerTotals(id: int):
+    frame = end.PlayerCareerStats(player_id=id).get_data_frames()[0]
+
+    gp = frame["GP"].sum()
+
+    stats = {"Points": round(frame["PTS"].sum()/gp, 1), "Assists": round(frame["AST"].sum()/gp, 1), "Rebounds": round(frame["REB"].sum()/gp, 1)}
+
+    return stats
+
+
+
+
+
+
+
